8141197373c9edce0525cdccfb07aad72cb77150
JazMac <jazmine.n.hudson@gmail.com>
2022-08-12 13:32:49 -0500

    inline and external.js with html

---
+++ js/javascript-with-html.js
@@ -0,0 +1,14 @@
+//alert("Yo!");
+
+// let userChoice = confirm("Are you sure you don't want to buy stuff?!");
+// if (userChoice) {
+//     alert ("Yay!")
+// } else {
+//     alert ("Boo :(");
+// }
+
+// console.log(userChoice)
+
+// let name added to create a function to store name
+let name = prompt("Whats's your name?");
+console.log(name)
